5d45863d4a3df305880131260f1f1a74df849465
Mickey wang <a0958062058@gmail.com>
2020-09-22 07:36:22 +0800

    calc.avg

--- read.py
+++ read.py
@@ -1,3 +1,4 @@
+#寫程式來讀取留言檔
 # 測試印出 data 資料
 #只印出第 0 筆看看
 #讀取檔案的過程中,印出 len(data) 才知道讀取進度
@@ -13,7 +14,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1 
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('------------------')
-print(data[1])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度為',sum_len/len(data))
